56123c5320cab8046a84144f8016bc6567f032b1
Carmen Romo <carmenromoluque@gmail.com>
2018-11-08 17:20:08 +0100

    Add load_zr_corrections function in dst_io file

--- antea/io/dst_io.py
+++ antea/io/dst_io.py
@@ -1,7 +1,9 @@
 import tables as tb
 import numpy  as np
 
-from invisible_cities.io. table_io    import make_table
+from invisible_cities.io  .table_io    import make_table
+from invisible_cities.io  .  dst_io    import load_dst
+from invisible_cities.reco.corrections import Correction
 
 
 class ZRfactors(tb.IsDescription):
@@ -39,3 +41,17 @@ def zr_correction_writer(hdf5_file, * ,
                      name         = table_name,
                      description  = "ZR corrections",
                      compression  = compression)
+
+
+def load_zr_corrections(filename, *,
+                        group = "Corrections",
+                        node  = "ZRcorrections",
+                        **kwargs):
+    dst  = load_dst(filename, group, node)
+    z, r = np.unique(dst.z.values), np.unique(dst.r.values)
+    f, u = dst.factor.values, dst.uncertainty.values
+
+    return Correction((z, r),
+                      f.reshape(z.size, r.size),
+                      u.reshape(z.size, r.size),
+                      **kwargs)
