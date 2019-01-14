2abe58c9f076bc601ca030394b7d0af5e7db5125
Carmen Romo <carmenromoluque@gmail.com>
2019-01-14 17:45:17 +0100

    Add sensor_functions and sensor_functions_test files

---
+++ antea/sensors/sensor_functions.py
@@ -0,0 +1,5 @@
+import numpy  as np
+import pandas as pd
+
+from invisible_cities.reco import wfm_functions    as wfm
+from invisible_cities.reco import sensor_functions as sf
---
+++ antea/sensors/sensor_functions_test.py
@@ -0,0 +1,6 @@
+import os
+import tables as tb
+import numpy  as np
+import pandas as pd
+from   pytest import mark
+
