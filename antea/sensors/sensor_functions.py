6e9c463d69464a44f635612a18bd347f1c049de9
Carmen Romo <carmenromoluque@gmail.com>
2019-01-14 17:47:35 +0100

    Add simulate_sipm_response function

--- antea/sensors/sensor_functions.py
+++ antea/sensors/sensor_functions.py
@@ -3,3 +3,28 @@ import pandas as pd
 
 from invisible_cities.reco import wfm_functions    as wfm
 from invisible_cities.reco import sensor_functions as sf
+
+
+def simulate_sipm_response(tot_charges, sipm_adc_to_pes, pe_resolution):
+    """ Return total signal in adc counts with the fluctuation of the pe.
+
+    Parameters
+    ----------
+    tot_charges     : np.array
+    Number of photoelectrons detected by each sipm.
+    sipm_adc_to_pes : np.array or float
+    Gain value of each sipm, or the same value for all of them.
+    pe_resolution   : float
+    Single photoelectron rms for sipms.
+
+    Returns
+    -------
+    charges_adc     : np.array
+    Total charges in adc counts.
+    """
+
+    ## Fluctuate according to charge resolution
+    sipm_fl    = sf.charge_fluctuation(tot_charges, pe_resolution)
+    charge_adc = sipm_fl * sipm_adc_to_pes
+    return charge_adc
+
